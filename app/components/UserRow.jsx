dfadc02a595342b8c2c313913ec5be0e41ee1e32
Zsolt Meszaros <zsolt@sowad.com>
2017-05-06 20:13:09 +0100

    Update dependencies

--- app/components/UserRow.jsx
+++ app/components/UserRow.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const UserRow = ({ username, img, recent, alltime }) => (
   <div className="board__row">
